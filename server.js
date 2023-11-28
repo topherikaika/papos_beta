32bf79f05cc7ddf1895fbb5b0cc1d43778749281
Christopher Ikaika Crespo <topherikaika@gmail.com>
2023-11-28 12:19:20 +0900

    move app listen to start function

--- server.js
+++ server.js
@@ -16,8 +16,7 @@ async function start() {
   const client = new MongoClient("mongodb://root:root@localhost:27017/papos_beta?&authSource=admin");
   await client.connect();
   db = client.db();
+  app.listen(3000);
 }
 
 start();
-
-app.listen(3000);
