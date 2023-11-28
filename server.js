3423fe4d98665f5fff28c566a877763d18e5bd4c
Christopher Ikaika Crespo <topherikaika@gmail.com>
2023-11-28 11:35:33 +0900

    create home and admin routes

--- server.js
+++ server.js
@@ -2,4 +2,12 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Welcome to the homepage");
+});
+
+app.get("/admin", (req, res) => {
+  res.send("This is the admin page");
+});
+
 app.listen(3000);
