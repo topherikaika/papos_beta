6b09372190516adaf14b450f40daa9c06b85c361
Christopher Ikaika Crespo <topherikaika@gmail.com>
2023-12-05 09:46:48 +0900

    create typicalReact to convert jsx to javascript

--- webpack.config.js
+++ webpack.config.js
@@ -1,2 +1,17 @@
 const nodeExternals = require("webpack-node-externals");
 const path = require("path");
+
+const typicalReact = {
+  rules: [
+    {
+      test: /\.js$/,
+      exclude: /(node_modules)/,
+      use: {
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-react"]
+        }
+      }
+    }
+  ]
+};
