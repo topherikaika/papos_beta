719be521daf1f4bd73f3871f5fd7c68d020b9bca
Christopher Ikaika Crespo <topherikaika@gmail.com>
2023-12-05 13:22:13 +0900

    create App function to render React element

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-alert("Test for webpack");
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+function App() {
+  return (
+    <div>
+      <h1>Hey!</h1>
+      <p>
+        <p>This is REACT</p>
+      </p>
+    </div>
+  );
+}
