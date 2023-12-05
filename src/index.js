22f5fda6a78ea1b3af9ae142f529c360cf8dd242
Christopher Ikaika Crespo <topherikaika@gmail.com>
2023-12-05 17:24:21 +0900

    remove useState and useImmer var

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import Axios from "axios";
 
 function App() {
-  const [recipes, setRecipes] = useState([]);
   return (
     <div>
       <h1>Hey!</h1>
