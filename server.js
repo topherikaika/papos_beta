eb64543f7bd4459946e3c6f68c7791776bc2372d
Christopher Ikaika Crespo <topherikaika@gmail.com>
2023-12-05 09:30:47 +0900

    create main.js file and alert to test

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ let db;
 const app = express();
 app.set("view engine", "ejs");
 app.set("views", "./views");
+app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
   const allRecipes = await db.collection("recipes").find().toArray();
