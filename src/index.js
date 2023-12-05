7b53d54ec0b5d76656a058f10a202e970edd5ede
Christopher Ikaika Crespo <topherikaika@gmail.com>
2023-12-05 13:37:14 +0900

    map recipes to return name and type keys

--- src/index.js
+++ src/index.js
@@ -7,8 +7,19 @@ function App() {
       <h1>Hey!</h1>
       <p>
         <p>This is REACT</p>
+        {recipes.map(function (recipe) {
+          return <RecipeCard name={recipe.name} type={recipe.type} />;
+        })}
       </p>
     </div>
+  );
+}
+
+function RecipeCard(props) {
+  return (
+    <p>
+      {props.name} is a dish from [] and is made of {props.type}
+    </p>
   );
 }
 
