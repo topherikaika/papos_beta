87249cb443d1b3b16431bd4c3d1977c5b476be8a
Christopher Ikaika Crespo <topherikaika@gmail.com>
2023-12-12 09:24:44 +0900

    create function AnimalCard and variables to use State: edit, draft, file and draft type

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,2 +1,9 @@
 import Axios from "axios";
 import React, { useState } from "react";
+
+function AnimalCard(props) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [draftName, setDraftName] = useState("");
+  const [file, setFile] = useState("");
+  const [draftType, setDraftType] = useState("");
+}
