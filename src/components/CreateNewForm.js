dc7c64093839150ca9ba083061fe7742f75c3a86
Christopher Ikaika Crespo <topherikaika@gmail.com>
2023-12-12 15:06:49 +0900

    create function CreateNewForm and nest variables for useState

--- src/components/CreateNewForm.js
+++ src/components/CreateNewForm.js
@@ -1,2 +1,9 @@
 import Axios from "axios";
 import React, { useState, useRef } from "react";
+
+function CreateNewForm(params) {
+  const [name, setName] = useState("");
+  const [type, setType] = useState("");
+  const [file, setFile] = useState("");
+  const CreatePhotoField = useRef();
+}
